43bdb361750ead3c6185eba8c161fe6784936e9e
RaspiestSheep3 <raspiestsheep3@hotmail.com>
2026-09-18 19:15:49 +0100

    Neuroevolution Implementation
    - Plan is to use standard Neuroevolution as opposed to NEAT
    - Pulled from old project, currently set to a < training
    - Bit of optimisation, more optimisation to be added

---
+++ Neuroevolution/Neuroevolution.cpp
@@ -0,0 +1,141 @@
+﻿// GeneticRouting.cpp : Defines the entry point for the application.
+//
+
+#include "Neuroevolution.h"
+#include "NeuroevolutionBase.h"
+
+using namespace std;
+
+//Settings
+int NUM_OF_MODELS = 100;
+int NUM_MODELS_FOR_CROSSOVER = 10;
+double MUTATION_CHANCE = 0.05;
+double MUTATION_SD = 0.01;
+int NUM_OF_ITERATIONS = 1000;
+
+random_device random;
+mt19937 generator(random());
+uniform_int_distribution uniformInt(0, NUM_MODELS_FOR_CROSSOVER - 1);
+
+//General variables
+int main()
+{
+	cout << "Test 2" << endl;
+
+	vector<unique_ptr<Model>> models;
+	vector<tuple<int, double>> outputs = {};
+
+	for (int i = 0; i < NUM_OF_MODELS; i++) {
+		auto model = make_unique<Model>();
+
+		model->AddDenseLayer(2, Activation::TANH);
+		
+		model->AddDenseLayer(16, Activation::SIGMOID);
+		model->AddDenseLayer(32, Activation::SIGMOID);
+		model->AddDenseLayer(32, Activation::SIGMOID);
+		model->AddDenseLayer(8, Activation::SIGMOID);
+		
+		//model->AddDenseLayer(3, "tanh");
+		model->AddDenseLayer(1, Activation::SIGMOID);
+
+		model->GenerateRawList();
+
+		models.push_back(std::move(model));
+		outputs.push_back({ i, 0 });
+	}
+
+	//For this genetic experiment, the model will take in 2 numbers between 0 and 1 ,a and b.
+	//Output 0 if a < b, output 1 if a >= b
+
+	vector<tuple<double, double, double>> inputs = {};
+
+	cout << "Data start" << endl;
+
+	double res;
+	for (double i = 0.1; i < 1; i += 0.1) {
+		for (double j = 0.1; j < 1; j += 0.1) {
+			if (i < j) res = 0;
+			else res = 1;
+
+			inputs.push_back({i, j, res});
+		}
+	}
+
+	cout << "Training start" << endl;
+
+	for (int h = 0; h < NUM_OF_ITERATIONS; h++) {
+		if ((h + 1) % 1 == 0) cout << "Iterations : " << h + 1 << "/" << NUM_OF_ITERATIONS << endl;
+
+		for (int i = 0; i < outputs.size(); i++) {
+			get<0>(outputs[i]) = i;
+		}
+
+		#pragma omp parallel for schedule(static)
+		for (int j = 0; j < models.size(); j++) {
+			vector<double> modelInputs(2);
+			vector<double> modelOutputs(1);
+			double offset = 0;
+
+			//Testing phase
+			for (const auto& set : inputs) {
+				modelInputs[0] = get<0>(set);
+				modelInputs[1] = get<1>(set);
+				models[j]->RunAlgorithm(&modelInputs, &modelOutputs);
+
+				offset += fabs(get<2>(set) - modelOutputs[0]);
+			}
+
+			get<1>(outputs[j]) = offset;
+		}
+
+		//Evolution phase
+		sort(outputs.begin(), outputs.end(),
+			[](const tuple<int, double>& a, const tuple<int, double>& b) { return get<1>(a) < get<1>(b); }
+		);
+
+		//cout << "Most effective score : " << get<1>(outputs[0]) << endl;
+
+		vector<unique_ptr<Model>> newGeneration = {};
+
+		int bestModelIdx = get<0>(outputs[0]);
+
+		//cout << "Best model index : " << bestModelIdx << endl;
+
+		newGeneration.push_back(models[bestModelIdx]->Clone());
+
+		//cout << outputs.size() << " Outputs size " << endl;
+
+		while(newGeneration.size() < NUM_OF_MODELS) {
+			int parent1Idx = get<0>(outputs[uniformInt(generator)]);
+			int parent2Idx = get<0>(outputs[uniformInt(generator)]);
+
+			while (parent2Idx == parent1Idx && NUM_MODELS_FOR_CROSSOVER > 1) {
+				parent2Idx = get<0>(outputs[uniformInt(generator)]);
+			}
+
+			auto child = models[parent1Idx]->Clone();
+
+			child->Crossover(models[parent2Idx].get(), MUTATION_CHANCE, MUTATION_SD);
+			child->GenerateRawList();
+
+			newGeneration.push_back(std::move(child));
+		}
+
+		models = std::move(newGeneration);
+	}
+
+	sort(outputs.begin(), outputs.end(),
+		[](const tuple<int, double>& a, const tuple<int, double>& b) { return get<1>(a) < get<1>(b); }
+	);
+
+	cout << "Lowest loss : " << get<1>(outputs[0]) << endl;
+
+	const auto filepath = filesystem::current_path() / "Models" / "1.json";
+
+	cout << "filepath exists?: " << filepath << endl;
+
+	models[get<0>(outputs[0])]->SaveModelWeights(filepath);
+	//for (auto output : outputs) cout << get<1>(output) << endl;
+
+	return 0;
+}
---
+++ Neuroevolution/NeuroevolutionBase.h
@@ -0,0 +1,222 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <random>
+#include <fstream>
+#include <nlohmann/json.hpp>
+#include <filesystem>
+
+using namespace std;
+using json = nlohmann::json;
+
+/*
+My goal is to make a generic neuroevolving model base, which can be adapted for whatever else I want to
+do.
+*/
+enum class Activation { TANH, SIGMOID };
+
+class Perceptron;
+
+class Edge {
+public:
+	Perceptron* start;
+	Perceptron* end;
+	double weight = 0.1;
+
+	Edge(Perceptron* startNode, Perceptron* endNode, double w)
+		: start(startNode), end(endNode), weight(w) {
+	}
+};
+
+class Perceptron {
+public:
+	vector<unique_ptr<Edge>> edges = {};
+	double bias = 0.1;
+	double result = 0.1;
+	Activation activationFunc;
+
+	Perceptron(Activation activation) : activationFunc(activation) {}
+
+	void ActivationFunction();
+
+private:
+	double CalculateSum();
+};
+
+class Model {
+public:
+	vector<vector<unique_ptr<Perceptron>>> layers = {}; //[0] is input layer, [numLayers - 1] is output layer
+	int GetNumLayers() const {
+		return layers.size();
+	}
+
+	vector<Perceptron*> rawPerceptrons = {};
+	vector<Edge*> rawEdges = {};
+
+	void RunAlgorithm(vector<double>* inputs, vector<double>* outputs);
+
+	void AddDenseLayer(int numNodes, Activation activationFunction);
+
+	void Crossover(Model* otherModel, double mutationChance, double mutationSD);
+
+	void GenerateRawList();
+
+	unique_ptr<Model> Clone();
+
+	void SaveModelWeights(filesystem::path filepath);
+	void LoadModelFromWeights(filesystem::path filepath);
+};
+
+using namespace std;
+
+random_device rd;
+mt19937 gen(rd());
+normal_distribution<double> norm(0.0, 1.0);
+uniform_real_distribution<double> uniform(0.0, 1.0);
+
+double Perceptron::CalculateSum() {
+	double sum = bias;
+
+	for (const auto& edge : edges) sum += edge->start->result * edge->weight;
+
+	return sum;
+}
+
+void Perceptron::ActivationFunction() {
+	double out = 0;
+
+	double sum = CalculateSum();
+	if (activationFunc == Activation::TANH) out = tanh(sum);
+	else if (activationFunc == Activation::SIGMOID) out = 1 / (1 + exp(-sum));
+
+	//else cout << "Activation function " << activationFunc << " unknown";
+
+	result = out;
+}
+
+void Model::RunAlgorithm(vector<double>* inputs, vector<double>* outputs) {
+	auto& inputLayer = layers[0];
+
+	for (int i = 0; i < inputLayer.size(); i++) inputLayer[i]->result = inputs->at(i);
+
+	for (int i = 1; i < layers.size(); i++) {
+		auto& targetLayer = layers[i];
+		for (int j = 0; j < targetLayer.size(); j++) targetLayer[j]->ActivationFunction();
+	}
+
+	int i = 0;
+	for (const auto& perceptron : layers.back()) {
+		outputs->at(i) = perceptron->result;
+	}
+}
+
+void Model::AddDenseLayer(int numPerceptrons, Activation activationFunction) {
+	vector<unique_ptr<Perceptron>> newLayer;
+
+	for (int i = 0; i < numPerceptrons; i++) {
+		auto perceptron = make_unique<Perceptron>(activationFunction);
+		perceptron->bias = norm(gen);
+
+		if (!layers.empty()) {
+			auto& previousLayer = layers.back();
+			for (const auto& previousPerceptron : previousLayer) {
+				double randomWeight = norm(gen);
+				perceptron->edges.push_back(make_unique<Edge>(previousPerceptron.get(), perceptron.get(), randomWeight));
+
+			}
+		}
+
+		newLayer.push_back(move(perceptron));
+	}
+
+	layers.push_back(move(newLayer));
+}
+
+void Model::Crossover(Model* otherModel, double mutationChance, double mutationSD) {
+	normal_distribution<double> mutator(0.0, mutationSD);
+
+	for (int i = 0; i < rawPerceptrons.size(); i++) {
+		if (uniform(gen) <= 0.5) rawPerceptrons[i]->bias = otherModel->rawPerceptrons[i]->bias;
+		if (uniform(gen) <= mutationChance) rawPerceptrons[i]->bias += mutator(gen);
+	}
+
+	for (int i = 0; i < rawEdges.size(); i++) {
+		if (uniform(gen) <= 0.5) rawEdges[i]->weight = otherModel->rawEdges[i]->weight;
+		if (uniform(gen) <= mutationChance) rawEdges[i]->weight += mutator(gen);
+	}
+}
+
+void Model::GenerateRawList() {
+	rawPerceptrons.clear();
+	rawEdges.clear();
+
+	for (auto& layer : layers) {
+		for (auto& perceptron : layer) {
+			rawPerceptrons.push_back(perceptron.get());
+
+			for (auto& edge : perceptron->edges) rawEdges.push_back(edge.get());
+		}
+
+	}
+}
+
+unique_ptr<Model> Model::Clone() {
+	//cout << "Clone 1 " << endl;
+
+	auto clonedModel = make_unique<Model>();
+
+	for (const auto& layer : layers) {
+		if (layer.empty()) continue;
+
+		Activation activationFunc = layer[0]->activationFunc;
+		clonedModel->AddDenseLayer(layer.size(), activationFunc);
+	}
+
+	clonedModel->GenerateRawList();
+
+	//cout << "Clone 2 " << endl;
+
+	//cout << rawEdges.size() << endl;
+
+	for (size_t i = 0; i < rawEdges.size(); i++) {
+		clonedModel->rawEdges[i]->weight = rawEdges[i]->weight;
+	}
+
+	//cout << "Clone 3" << endl;
+
+	for (size_t i = 0; i < rawPerceptrons.size(); i++) {
+		clonedModel->rawPerceptrons[i]->bias = rawPerceptrons[i]->bias;
+	}
+	return clonedModel;
+}
+
+void Model::SaveModelWeights(filesystem::path filepath) {
+	json modelJSON = {
+		{"Perceptron Biases", {}},
+		{"Edge Weights", {}}
+	};
+
+	for (size_t i = 0; i < rawPerceptrons.size(); i++) modelJSON["Perceptron Biases"].push_back(rawPerceptrons[i]->bias);
+	for (size_t i = 0; i < rawEdges.size(); i++) modelJSON["Edge Weights"].push_back(rawEdges[i]->weight);
+
+	ofstream file(filepath);
+
+	if (!file.is_open()) {
+		cerr << "ERROR: Failed to open file." << endl;
+		return;
+	}
+
+	file << modelJSON.dump(4);
+	file.close();
+}
+
+void Model::LoadModelFromWeights(filesystem::path filepath) {
+	ifstream file(filepath);
+	json modelJSON = json::parse(file);
+	file.close();
+
+	for (int i = 0; i < rawPerceptrons.size(); i++) rawPerceptrons[i]->bias = modelJSON["Perceptron Biases"][i];
+	for (int i = 0; i < rawEdges.size(); i++) rawEdges[i]->weight = modelJSON["Edge Weights"][i];
+}
