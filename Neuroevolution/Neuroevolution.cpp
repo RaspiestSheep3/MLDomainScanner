f82bf6a670268de4ca4a41f2adbc9a9636a03427
RaspiestSheep3 <raspiestsheep3@hotmail.com>
2026-09-20 11:14:41 +0100

    Evaluation, Architecture change
    - Evaluation function to objectively test models
    - Changed to 6-12-6-1 from 6-12-1

--- Neuroevolution/Neuroevolution.cpp
+++ Neuroevolution/Neuroevolution.cpp
@@ -11,10 +11,12 @@ using namespace std::chrono;
 int NUM_OF_MODELS = 100;
 int NUM_MODELS_FOR_CROSSOVER = 10;
 double MUTATION_CHANCE = 0.1;
-double MUTATION_SD = 0.1;
-int NUM_OF_ITERATIONS = 10000;
-int NUM_ENTRIES_FOR_TRAINING = 500;
+double MUTATION_SD = 0.05;
+int NUM_OF_ITERATIONS = 20000;
+int NUM_ENTRIES_PER_TRAIN_STEP = 500;
 int GENERATIONS_PER_SHUFFLE = 50;
+int NUM_ENTRIES_FOR_TRAINING = 90000;
+int NUM_ENTRIES_FOR_EVALUATION = 10000;
 
 //Database input
 string dbPath;
@@ -30,14 +32,56 @@ mt19937 generator(random());
 uniform_int_distribution uniformInt(0, NUM_MODELS_FOR_CROSSOVER - 1);
 auto rng = default_random_engine{};
 
-//General variables
-int main()
-{
-	cout << "Database Path : ";
-	cin >> dbPath;
+vector<DomainEntry> testingResults;
 
+double Evaluate(Model* model) {
+	vector<double> modelInputs = {};
+	vector<double> modelOutputs = { 0 };
+	double offset = 0;
+
+	//Testing phase
+	//cout << "Marker 1" << endl;
+
+	auto start = high_resolution_clock::now();
+	for (int i = 0; i < NUM_ENTRIES_FOR_EVALUATION; i++) {
+		if (modelInputs.size() > 0) modelInputs.clear();
+		if (modelOutputs.size() > 1) modelOutputs = { 1 };
+
+		//cout << "Marker 2" << endl;
+
+		const auto& set = testingResults[i];
+
+		for (int k = 0; k < 6; k++) modelInputs.push_back(set.features[k]);
+
+		//cout << "Marker 2.1" << endl;
+		//cout << modelInputs.size() << endl;
+
+		model->RunAlgorithm(&modelInputs, &modelOutputs);
+
+		//cout << "Marker 3" << endl;
+
+		offset += fabs(set.isMalicious - modelOutputs[0]);
+	}
+	auto end = high_resolution_clock::now();
+	duration<double, milli> duration = end - start;
+
+	/*cout << "Testing Results : " << "\n"
+		<< " Average Offset : " << offset / NUM_ENTRIES_FOR_EVALUATION << "\n"
+		<< " Time Taken     : " << duration << endl;
+	*/
+
+	return offset / NUM_ENTRIES_FOR_EVALUATION;
+}
+
+void Train()
+{
 	vector<unique_ptr<Model>> models;
 	vector<tuple<int, double>> outputs = {};
+
+	string loadBuffer;
+	cout << "Load old models? (Y/N) : ";
+	cin >> loadBuffer;
+	auto filepath = filesystem::current_path() / "Models" / "1.json";
 
 	for (int i = 0; i < NUM_OF_MODELS; i++) {
 		auto model = make_unique<Model>();
@@ -50,10 +94,16 @@ int main()
 		model->AddDenseLayer(8, Activation::SIGMOID);*/
 		
 		model->AddDenseLayer(12, Activation::SIGMOID);
+		model->AddDenseLayer(6, Activation::SIGMOID);
 
 		model->AddDenseLayer(1, Activation::SIGMOID);
 
 		model->GenerateRawList();
+
+		if (loadBuffer == "Y") {
+			filepath = filesystem::current_path() / "Models" / "Training" / (to_string(i) + ".json");
+			model->LoadModelFromWeights(filepath);
+		}
 
 		models.push_back(std::move(model));
 		outputs.push_back({ i, 0 });
@@ -68,31 +118,17 @@ int main()
 	if (sqlite3_open(dbPath.c_str(), &db) != SQLITE_OK) {
 		std::cerr << "Error opening database: " << sqlite3_errmsg(db) << std::endl;
 		sqlite3_close(db);
-		return 1;
 	}
 
-	sqlite3_stmt* countStmt;
-	const char* countSql = "SELECT COUNT(*) FROM domains;";
-	size_t totalRows = 0;
+	inputs.reserve(NUM_ENTRIES_FOR_TRAINING);
 
-	if (sqlite3_prepare_v2(db, countSql, -1, &countStmt, nullptr) == SQLITE_OK) {
-		if (sqlite3_step(countStmt) == SQLITE_ROW) {
-			totalRows = static_cast<size_t>(sqlite3_column_int64(countStmt, 0));
-		}
-		sqlite3_finalize(countStmt);
-	}
-
-	if (totalRows > 0) {
-		inputs.reserve(totalRows);
-	}
-
-	const char* query = "SELECT shannonEntropy, vowelConsonantRatio, longestConsecutiveConsonants, dictionaryCount, bigramCount, trigramCount, isMalicious FROM domains";
+	string queryStr = ("SELECT shannonEntropy, vowelConsonantRatio, longestConsecutiveConsonants, dictionaryCount, bigramCount, trigramCount, isMalicious FROM domains ORDER BY domain ASC LIMIT " + to_string(NUM_ENTRIES_FOR_TRAINING));
+	const char* query = queryStr.c_str();
 	sqlite3_stmt * stmt;
 
 	if (sqlite3_prepare_v2(db, query, -1, &stmt, nullptr) != SQLITE_OK) {
 		std::cerr << "Failed to prepare query: " << sqlite3_errmsg(db) << std::endl;
 		sqlite3_close(db);
-		return 1;
 	}
 
 	while (sqlite3_step(stmt) == SQLITE_ROW) {
@@ -137,7 +173,7 @@ int main()
 			//Testing phase
 			//cout << "Marker 1" << endl;
 
-			for (int i = 0; i < NUM_ENTRIES_FOR_TRAINING; i++) {
+			for (int i = 0; i < NUM_ENTRIES_PER_TRAIN_STEP; i++) {
 				if (modelInputs.size() > 0) modelInputs.clear();
 				if (modelOutputs.size() > 1) modelOutputs = { 1 };
 
@@ -206,7 +242,7 @@ int main()
 			duration<double, std::milli> evolutionDuration = evolutionEnd - testingEnd;
 
 			cout << "Iteration : " << h + 1 << "/" << NUM_OF_ITERATIONS
-				<< " | Best Loss : " << get<1>(outputs[0]) << "\n"
+				<< " | Best Loss : " << Evaluate(models[bestModelIdx].get()) << "\n"
 				<< "  -> Testing: " << testingDuration.count() << " ms\n"
 				<< "  -> Evolution: " << evolutionDuration.count() << " ms" << endl;
 		}
@@ -216,14 +252,84 @@ int main()
 		[](const tuple<int, double>& a, const tuple<int, double>& b) { return get<1>(a) < get<1>(b); }
 	);
 
-	cout << "Lowest loss : " << get<1>(outputs[0]) << endl;
-
-	const auto filepath = filesystem::current_path() / "Models" / "1.json";
+	filepath = filesystem::current_path() / "Models" / "1.json";
 
 	cout << "filepath exists?: " << filepath << endl;
 
 	models[get<0>(outputs[0])]->SaveModelWeights(filepath);
-	//for (auto output : outputs) cout << get<1>(output) << endl;
 
+	//We have to save all models for later training
+	
+	for (int i = 0; i < NUM_OF_MODELS; i++) {
+		filepath = filesystem::current_path() / "Models" / "Training" / (to_string(i) + ".json");
+		models[get<0>(outputs[i])]->SaveModelWeights(filepath);
+	}
+}
+
+int main() {
+	//Setup for training
+	cout << "Database Path : ";
+	cin >> dbPath;
+
+	sqlite3* db;
+
+	if (sqlite3_open(dbPath.c_str(), &db) != SQLITE_OK) {
+		std::cerr << "Error opening database: " << sqlite3_errmsg(db) << std::endl;
+		sqlite3_close(db);
+	}
+
+	testingResults.reserve(NUM_ENTRIES_FOR_EVALUATION);
+
+	string queryStr = ("SELECT shannonEntropy, vowelConsonantRatio, longestConsecutiveConsonants, dictionaryCount, bigramCount, trigramCount, isMalicious FROM domains ORDER BY domain DESC LIMIT " + to_string(NUM_ENTRIES_FOR_EVALUATION));
+	const char* query = queryStr.c_str();
+	sqlite3_stmt* stmt;
+
+	if (sqlite3_prepare_v2(db, query, -1, &stmt, nullptr) != SQLITE_OK) {
+		std::cerr << "Failed to prepare query: " << sqlite3_errmsg(db) << std::endl;
+		sqlite3_close(db);
+	}
+
+	while (sqlite3_step(stmt) == SQLITE_ROW) {
+		const unsigned char* domain = sqlite3_column_text(stmt, 0);
+
+		DomainEntry result;
+
+		result.features[0] = static_cast<float>(sqlite3_column_double(stmt, 0));
+		result.features[1] = static_cast<float>(sqlite3_column_double(stmt, 1));
+		result.features[2] = static_cast<float>(sqlite3_column_double(stmt, 2));
+		result.features[3] = static_cast<float>(sqlite3_column_double(stmt, 3));
+		result.features[4] = static_cast<float>(sqlite3_column_double(stmt, 4));
+		result.features[5] = static_cast<float>(sqlite3_column_double(stmt, 5));
+		result.isMalicious = static_cast<float>(sqlite3_column_int(stmt, 6));
+
+		testingResults.push_back(result);
+	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_close(db);
+
+	string buffer;
+	cout << "Train mode? (Y/N) : ";
+	cin >> buffer;
+
+	if (buffer == "Y") Train();
+	else {
+		auto model = make_unique<Model>();
+
+		model->AddDenseLayer(6, Activation::TANH);
+
+		model->AddDenseLayer(12, Activation::SIGMOID);
+		model->AddDenseLayer(6, Activation::SIGMOID);
+
+		model->AddDenseLayer(1, Activation::SIGMOID);
+
+		model->GenerateRawList();
+
+		model->LoadModelFromWeights(filesystem::current_path() / "Models" / "1.json");
+
+		double avgOffset = Evaluate(model.get());
+
+		cout << "Average offset : " << avgOffset << endl;
+	}
 	return 0;
 }
